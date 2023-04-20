22ea9363454c789600c560ecfc9c8a014b83dc3e
HEEJIN LIM <127469548+xeexin@users.noreply.github.com>
2023-04-20 20:51:18 +0900

    Update app.js

--- app.js
+++ app.js
@@ -5,5 +5,16 @@ function handleTitleClick(){
     title.style.color = "blue";
     console.log("title was clicked");
 }
+function handlemouseEnter(){
+    title.innerText="Mouse is here";
+    console.log("Mouse is here!");
+}
+function handleMouseLeave(){
+    title.innerText = "Mouse is gone!";
+}
 
 title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handlemouseEnter);
+title.addEventListener("mouseleave",handleMouseLeave);
+
+//MDL : https://developer.mozilla.org/en-US/docs/Web/API/HTMLHeadingElement
