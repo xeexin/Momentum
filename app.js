2c006f9eeba351480c3d68ec6bcb1271b3c27727
HEEJIN LIM <127469548+xeexin@users.noreply.github.com>
2023-04-20 21:09:15 +0900

    Event

--- app.js
+++ app.js
@@ -1,20 +1,44 @@
-const title = document.querySelector("div.hello:first-child h1");
-console.dir(title);
+const h1 = document.querySelector("div.hello:first-child h1");
+console.dir(h1);
 
-function handleTitleClick(){
-    title.style.color = "blue";
-    console.log("title was clicked");
+function handleh1Click(){
+    h1.style.color = "blue";
+    console.log("h1 was clicked");
 }
 function handlemouseEnter(){
-    title.innerText="Mouse is here";
+    h1.innerText="Mouse is here";
     console.log("Mouse is here!");
 }
 function handleMouseLeave(){
-    title.innerText = "Mouse is gone!";
+    h1.innerText = "Mouse is gone!";
 }
 
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handlemouseEnter);
-title.addEventListener("mouseleave",handleMouseLeave);
 
 //MDL : https://developer.mozilla.org/en-US/docs/Web/API/HTMLHeadingElement
+
+h1.addEventListener("click", handleh1Click);
+//h1.onclick = handleh1Click;
+
+h1.addEventListener("mouseenter", handlemouseEnter);
+//h1.onmouseenter = handlemouseEnter;
+
+h1.addEventListener("mouseleave",handleMouseLeave);
+//h1.onmouseleave = handleMouseLeave;
+
+function handleWindowResize(){
+    document.body.style.backgroundColor= "tomato";
+}
+
+function handleWindowCopy(){
+    alert("Copier!");
+}
+function handleWindowOffline(){
+    alert("no wifi :(");
+}
+function handleWindowOnline(){
+    alert("All Good :)");
+}
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
