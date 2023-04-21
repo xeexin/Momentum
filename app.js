66fe7893c8c08d63d2c028662053da633d875324
HEEJIN LIM <127469548+xeexin@users.noreply.github.com>
2023-04-21 18:10:04 +0900

    classList.toggle

--- app.js
+++ app.js
@@ -2,12 +2,7 @@ const h1 = document.querySelector("div.hello:first-child h1");
 console.dir(h1);
 
 function handleH1Click(){
-  const clickedClass = "clicked";
-  if(h1.classList.contains(clickedClass)){
-    h1.classList.remove(clickedClass);
-} else{
- h1.classList.add(clickedClass);
-}    
+    h1.classList.toggle("clicked"); 
 }
 
 
