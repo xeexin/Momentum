770d4143088d2d188e6598de13ccf257639eeef7
HEEJIN LIM <127469548+xeexin@users.noreply.github.com>
2023-04-24 01:05:36 +0900

    4.6 Loading Username

--- app.js
+++ app.js
@@ -2,17 +2,32 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
 const greeting = document.querySelector("#greeting");
+
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function paintGreetings(name){
+    greeting.innerText = `Hello ${name}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  localStorage.setItem("username",username);
+  localStorage.setItem(USERNAME_KEY,username);
   console.log(username);
-  greeting.innerText = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+  paintGreetings(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+
+if( savedUserName === null) {
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else{
+    // show h1
+    paintGreetings(savedUserName);
+}
 
