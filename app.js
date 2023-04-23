3ccc56d696c3e44ba812fa41498471e0548be461
HEEJIN LIM <127469548+xeexin@users.noreply.github.com>
2023-04-23 15:10:18 +0900

    4.2 Events

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const loginForm = document.getElementById("login-form");
 const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
 
 // const loginInput = document.querySelector("#login-form input");
 // const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtn(){
-    const userName = loginInput.value;
+function onLoginSubmit(event){
+  event.preventDefault();
+  console.log(loginInput.value);
 
 }
 
-loginButton.addEventListener("click", onLoginBtn)
+loginForm.addEventListener("submit", onLoginSubmit);
