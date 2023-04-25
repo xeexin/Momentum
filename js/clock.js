f94c1c67972e726ef34c78fa23a7739322348fec
HEEJIN LIM <127469548+xeexin@users.noreply.github.com>
2023-04-25 14:01:27 +0900

    5.0 setInterval

---
+++ js/clock.js
@@ -0,0 +1,7 @@
+const clock = document.querySelector("h2#clock");
+
+function sayHello(){
+    console.log("HELLO");
+}
+
+setInterval(sayHello, 5000);
--- js/greeting.js
+++ js/greeting.js
@@ -14,6 +14,7 @@ function paintGreetings(){
 
 function onLoginSubmit(event) {
   event.preventDefault();
+  const username = loginInput.value;
   loginForm.classList.add(HIDDEN_CLASSNAME);
   localStorage.setItem(USERNAME_KEY,username);
   console.log(username);
