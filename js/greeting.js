e1074f643e2062ffa25ae8c1180985a4a9065b24
HEEJIN LIM <127469548+xeexin@users.noreply.github.com>
2023-04-29 19:37:00 +0900

    7.0 Setup

--- js/greeting.js
+++ js/greeting.js
@@ -6,19 +6,18 @@ const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
-function paintGreetings(){
-    const username = localStorage.getItem("username");
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
-}
 
 function onLoginSubmit(event) {
-  event.preventDefault();
-  const username = loginInput.value;
-  loginForm.classList.add(HIDDEN_CLASSNAME);
-  localStorage.setItem(USERNAME_KEY,username);
-  console.log(username);
-  paintGreetings();
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY,username);
+    paintGreetings(username);
+}
+
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUserName = localStorage.getItem(USERNAME_KEY);
@@ -29,6 +28,6 @@ if( savedUserName === null) {
     loginForm.addEventListener("submit", onLoginSubmit);
 } else{
     // show h1
-    paintGreetings();
+    paintGreetings(savedUserName);
 }
 
