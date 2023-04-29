eb1f6f3b775dc2a195abd0cc1c1d530dccb762e6
HEEJIN LIM <127469548+xeexin@users.noreply.github.com>
2023-04-30 01:08:19 +0900

    Saving ToDos
    JSON.stringify()

--- js/todo.js
+++ js/todo.js
@@ -3,6 +3,14 @@ const toDoInput = toDoForm.querySelector("input");
 
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+//저장
+function saveToDo(){
+    localStorage.setItem("todos", JSON.stringify(toDos));
+}
+
+//삭제
 function deleteTodo(event){
     //console.dir(event.target.parentElement.innerText);
     //console.log(event.target.parentElement);
@@ -10,6 +18,7 @@ function deleteTodo(event){
     li.remove();
 }
 
+//화면에 리스트 보이기
 function paintToDo(newTodo){
     const li = document.createElement("li");
     const span = document.createElement("span");
@@ -23,11 +32,14 @@ function paintToDo(newTodo){
 
 }
 
+//엔터 키 눌렀을 때
 function handleToDoSubmit(event){
     event.preventDefault();
     const newToDo = toDoInput.value;
-    toDoInput.value = "";
+    toDoInput.value = ""; //입력 후 공백 만들기
+    toDos.push(newToDo);
     paintToDo(newToDo);
+    saveToDo(newToDo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
